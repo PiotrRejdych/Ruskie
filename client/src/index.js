5f9c530c112e9d01a04ab0204ab98e15fa0875eb
Krzysztof Perwel <perkristo@gmail.com>
2018-07-20 21:19:54 +0200

    Remove client network code

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,6 @@
 import { Game, Scene, AUTO } from "phaser";
 import avatarImage from "./avatar.png";
 
-import { Client } from "colyseus.js";
-const client = new Client("ws://127.0.0.1:2657");
 class MainScene extends Scene {
   constructor() {
     super("mainScene");
@@ -12,26 +10,7 @@ class MainScene extends Scene {
     this.load.image("avatar", avatarImage);
   }
   create() {
-    const room = client.join("game");
-
-    room.listen("players/:id", ({ path: { id }, operation, value }) => {
-      if (operation === "add") {
-        this.avatars[id] = this.add.image(value.x, value.y, "avatar");
-      }
-      if (operation === "remove") {
-        this.avatars[id].destroy();
-      }
-    });
-
-    room.listen("players/:id/:attribute", ({ path: { id, attribute }, value, operation }) => {
-      if (operation === "replace") {
-        this.avatars[id][attribute] = value;
-      }
-    });
-
-    this.input.on("pointermove", ({ x, y }) => {
-      room.send({ action: "mousemove", x, y });
-    });
+    this.avatars = this.add.image(10, 10, "avatar");
   }
 }
 
