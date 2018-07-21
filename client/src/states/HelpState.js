ad157c66f81353e1713336d5ccb1967ed9b28c22
Maciej <tyrala.maciej@gmail.com>
2018-07-21 21:39:43 +0200

    custom font

---
+++ client/src/states/HelpState.js
@@ -0,0 +1,18 @@
+import { Button } from "../components/Button";
+
+export class MenuState extends Phaser.State {
+    create() {
+        const playButton = this._createPlayButton();
+    }
+
+    _createPlayButton() {
+        const button = new Button(this.game, "2 players", this._proceedToGameScene);
+        button.x = this.game.width * 0.5;
+        button.y = this.game.height * 0.5;
+        return button;
+    }
+
+    _proceedToGameScene() {
+        this.game.state.start('Game', true, false, 3);
+    }
+}
