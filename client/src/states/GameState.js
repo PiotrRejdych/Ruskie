8184969c6abebbb2054a0c8a4f7ea15b1af02a92
Krzysztof Perwel <perkristo@gmail.com>
2018-07-21 18:18:15 +0200

    set fillness to fullness

--- client/src/states/GameState.js
+++ client/src/states/GameState.js
@@ -1,12 +1,14 @@
 import { GameModel } from "../model/GameModel";
 import { GameView } from "../view/GameView";
 import { GameController } from "../controller/GameController";
+import PlayerController from '../controller/PlayerController';
 
 export class GameState extends Phaser.State {
 	init(numberOfPlayers) {
-		const model = new GameModel(this.game.cache, numberOfPlayers);
-		const view = new GameView(this.game);
-		this._controller = new GameController(model, view);
+		const gameModel = new GameModel(this.game.cache, numberOfPlayers);
+		const gameView = new GameView(this.game);
+
+		this._controller = new GameController(gameModel, gameView);
 	}
 
 	create() {
