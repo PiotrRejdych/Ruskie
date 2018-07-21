889add0da9bc52b71aa46a089634fc1a6ee018a5
eRedin <piotr.rejdych@gmail.com>
2018-07-21 18:36:18 +0200

    FoodItem class

--- client/src/view/Conveyor.js
+++ client/src/view/Conveyor.js
@@ -1,5 +1,12 @@
 export class Conveyor extends Phaser.Group {
-	constructor(game) {
+	constructor(game, x, y) {
 		super(game);
+
+		this.x = x;
+		this.y = y;
+
+		this.backgroundImage = this.game.add.sprite(this.x, this.y, 'conveyorBelt');
+		this.backgroundImage.scale.setTo(0.3, 0.3);
 	}
+
 }
---
+++ client/src/view/FoodItem.js
@@ -0,0 +1,6 @@
+export class FoodItem extends Phaser.Sprite() {
+    constructor(game) {
+        super(game, 0, 0, 'dumpling1');
+
+    }
+}
