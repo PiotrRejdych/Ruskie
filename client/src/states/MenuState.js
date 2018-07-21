ce24c7f70e9edf90cc45796b3784d539dd1af72e
Maciej <tyrala.maciej@gmail.com>
2018-07-21 23:24:28 +0200

    exit button

--- client/src/states/MenuState.js
+++ client/src/states/MenuState.js
@@ -5,7 +5,10 @@ export class MenuState extends Phaser.State {
         this.bg = this.game.add.tileSprite(0, 0, this.game.width, this.game.cache.getImage('background').height, 'background');
         const playButton = this._createPlayButton();
         const creditsButton = this._createCreditsButton();
-	}
+        const exitButton = this._createExitButton();
+        this.game.add.text(this.game.width / 2 - 350, 100, "Ruskie", {font:"300px Bulgaria_Moderna", fill:"#000000"});
+
+    }
 
 	_createPlayButton() {
 		const button = new Button(this.game, "2 players", this._proceedToGameScene);
@@ -21,11 +24,22 @@ export class MenuState extends Phaser.State {
         return button;
     }
 
+    _createExitButton() {
+        const button = new Button(this.game, "Exit", this._closeTab);
+        button.x = this.game.width * 0.5;
+        button.y = this.game.height * 0.5 + 220;
+        return button;
+    }
+
 	_proceedToGameScene() {
 		this.game.state.start('Game', true, false, 3);
 	}
 
     _proceedToCreditsScene() {
         this.game.state.start('Credits', true, false, 3);
+    }
+
+    _closeTab() {
+	    close();
     }
 }
