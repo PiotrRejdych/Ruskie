74d678d84d07ec0469694d9789789a51450158b4
eRedin <piotr.rejdych@gmail.com>
2018-07-22 12:42:19 +0200

    Game speed twitching

--- client/src/controller/GameController.js
+++ client/src/controller/GameController.js
@@ -79,7 +79,7 @@ export class GameController {
 
     _toEatOrNotToEat(playerIndex, dish) {
 		if(this._model.isPlayerEating(playerIndex)) {
-			let sating = dish.sating * 20;
+			let sating = dish.sating * 1;
 
 			if (this._model.isPlayerAllergicToDish(playerIndex, dish)) {
 				sating *= 0.5;
